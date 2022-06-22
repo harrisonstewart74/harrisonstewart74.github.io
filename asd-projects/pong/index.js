b74533316a474a9b2890ffc0b4c68bec2a450be2
harrisonstewart74 <harrisonstewart74@gmail.com>
2022-06-22 19:22:48 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -6,15 +6,19 @@ function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  
+
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const MAX_SPEED = 10;
+  const SPEED_INCREMENT = .15;
+  const SPEED_RATIO = .1;
+
 
   // Game Item Objects
   var ball = GameItem('#ball')
   var paddle = GameItem('#paddle');
-
+  var storeBricks = [];
   var KEY = {
     "left": 37,
     "right": 39,
@@ -38,6 +42,7 @@ function runProgram() {
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);
   startball();
+  creation();
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -53,19 +58,20 @@ function runProgram() {
   function newFrame() {
     movePaddle();
     moveBall();
-
+    handlePaddleCollision();
+    allBrickCollisions();
   }
-  function moveBall(){
-    ball.x += ball.speedX;             
+  function moveBall() {
+    ball.x += ball.speedX;
     if (ball.x + ball.width >= boardWidth) {
       ball.speedX = ball.speedX * -1;
     }
     if (ball.x < 0) {
       ball.speedX = ball.speedX * -1;
     }
-  // update the position of the box along the x-axis
+    // update the position of the box along the x-axis
     ball.y += ball.speedY
-  if (ball.y + ball.height >= boardHeight) {
+    if (ball.y + ball.height >= boardHeight) {
       ball.speedY = ball.speedY * -1;
     }
     if (ball.y < 0) {
@@ -75,7 +81,56 @@ function runProgram() {
     $(ball.id).css("top", ball.y)
 
   }
+  function doCollide(obj1, obj2) {
+    // TODO: calculate and store the remaining
+    // sides of the obj1
+    obj1.leftX = obj1.x;
+    obj1.topY = obj1.y;
+    obj1.rightX = obj1.x + obj1.width
+    obj1.bottomY = obj1.y + obj1.height
+    // TODO: Do the same for obj2
+    obj2.leftX = obj2.x;
+    obj2.topY = obj2.y;
+    obj2.rightX = obj2.x + obj2.width
+    obj2.bottomY = obj2.y + obj2.height
 
+    if (obj1.leftX < obj2.rightX && obj1.bottomY > obj2.topY && obj1.rightX > obj2.leftX && obj1.topY < obj2.bottomY) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  function allBrickCollisions() {
+    for (var i = 0;i < storeBricks.length;i++){
+     if (doCollide(ball,storeBricks[i] )){
+      handleBrickCollisions(storeBricks[i]);
+      $(storeBricks[i].id).remove();
+      storeBricks.splice(i, 1);
+     }
+    }
+  }
+
+  function handlePaddleCollision() {
+    if (doCollide(paddle, ball)) {
+      ball.speedY = ball.speedY * -1;
+
+      var ballMiddle = (ball.x + ball.width / 2);
+
+      var paddleMiddle = (paddle.x + paddle.width / 2);
+
+      ball.speedX += (ballMiddle - paddleMiddle) * SPEED_RATIO;
+
+      ball.speedY += SPEED_INCREMENT;
+
+      ball.speedX = Math.min(ball.speedX, MAX_SPEED);
+      ball.speedX = Math.max(ball.speedX, -MAX_SPEED);
+      ball.speedY = Math.min(ball.speedY, MAX_SPEED);
+      ball.speedY = Math.max(ball.speedY, -MAX_SPEED);
+
+    }
+  }
   /* 
   Called in response to events.
   */
@@ -114,7 +169,7 @@ function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  function startball(){
+  function startball() {
     ball.speedX = randomNum = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
     ball.speedY = randomNum = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
     ball.x = 0
@@ -129,4 +184,47 @@ function runProgram() {
     $(document).off();
   }
 
+  function creation() {
+    var leftFirst = 40;
+    var brickX;
+    var brickY = 40
+    for (var i = 0; i < 9; i++) {
+      brickX = leftFirst;
+      for (var j = 0; j < 9; j++) {
+        var nameTheBrick = "theBricks" + storeBricks.length;
+        var brickElement = $('<div>')
+          .appendTo($('#board'))
+          .addClass("brick")
+          .css("left", brickX)
+          .css("top", brickY)
+          .attr("id", nameTheBrick );
+
+        var brick = GameItem('#' + nameTheBrick);
+        storeBricks.push(brick);
+        brickX += 40;
+      }
+      brickY += 20;
+    }
+  }
+
+  function handleBrickCollisions(boxOfBrick) {
+    var ballMiddleX = (ball.x + ball.width / 2);
+    var brickMiddleX = (boxOfBrick.x + boxOfBrick.width / 2);
+    var ballMiddleY = (ball.y + ball.height / 2);
+    var brickMiddleY = (boxOfBrick.y + boxOfBrick.height / 2);
+
+    var ballRelX = ballMiddleX - brickMiddleX;
+    var ballRelY = ballMiddleY - brickMiddleY;
+
+    var ballDistancePercentX = ballRelX / boxOfBrick.width;
+    var ballDistancePercentY = ballRelY / boxOfBrick.height;
+
+    if (Math.abs(ballDistancePercentX) < Math.abs(ballDistancePercentY)){
+      ball.speedY *= -1;
+    }
+    else {
+      ball.speedX *= -1;
+    }
+  }
 }
+
